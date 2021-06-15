1b73c0301429bd8a208c8b0652440ea9218bca04
Konstantin Aladyshev <aladyshev22@gmail.com>
2021-06-15 18:21:58 +0300

    Add source code for lessons 6 and 7

---
+++ Lesson_6/UefiLessonsPkg/ImageInfo/ImageInfo.c
@@ -0,0 +1,29 @@
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiLib.h>
+
+
+typedef struct {
+  UINTN               Signature;
+  /// All handles list of IHANDLE
+  LIST_ENTRY          AllHandles;
+  /// List of PROTOCOL_INTERFACE's for this handle
+  LIST_ENTRY          Protocols;
+  UINTN               LocateRequest;
+  /// The Handle Database Key value when this handle was last created or modified
+  UINT64              Key;
+} IHANDLE;
+
+EFIAPI
+UefiMain (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  IHANDLE* MyHandle = ImageHandle;
+  Print(L"Signature: %c %c %c %c\n", (MyHandle->Signature >>  0) & 0xff,
+                                     (MyHandle->Signature >>  8) & 0xff,
+                                     (MyHandle->Signature >> 16) & 0xff,
+                                     (MyHandle->Signature >> 24) & 0xff);
+
+  return EFI_SUCCESS;
+}
